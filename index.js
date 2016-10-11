87deac8c0ec3b9d44fe2fe4f4790062ede6dfc65
Boelensman1 <me@wiggerboelens.com>
2016-10-11 20:22:45 +0200

    Reformatted & added functions of idlesign/deluge-webapi
    Signed-off-by: Boelensman1 <me@wiggerboelens.com>

--- index.js
+++ index.js
@@ -1,186 +1,241 @@
 /**
  * Created by nusrathkhan on 2015/10/20.
  */
-var http = require('http');
-var zlib = require('zlib');
-var schedule = require('node-schedule');
-var Q = require("q");
+const http = require('http');
+const zlib = require('zlib');
+const schedule = require('node-schedule');
+const Q = require('q');
 
-module.exports = function deluge(hostname, password, port){
+module.exports = function deluge(hostname, password, port) {
 
-	// INIT vars
-	if(port == null){
-		port = 8112;
-	}
+  // INIT consts
+  if(port == null) {
+    port = 8112;
+  }
 
-	var counter;
-	var auth_cooke = "";
-
-
+  let counter;
+  let authCookie = '';
 
 
-	//update cookie every hour, as it expires
-	var j = schedule.scheduleJob('0 * * * *', function(){
-		auth(loggit);
-	});
 
-	function close() {
-		j.cancel();
-	}
 
-	function get_config()
-	{
-		var toR = Q.defer();
-		var params = [];
-		send_request("core.get_config", params, function(data){
-			if (data) {
-				toR.resolve(data);
-			} else {
-				toR.reject(data);
-			}
-		});
+  //update cookie every hour, as it expires
+  const j = schedule.scheduleJob('0 * * * *', function() {
+    auth(loggit);
+  });
 
-		return toR.promise;
-	}
+  function close() {
+    j.cancel();
+  }
 
-	function get_status(callback)
-	{
-		var toR = Q.defer();
-		var params;
+  function getConfig() {
+    const toR = Q.defer();
+    const params = [];
+    sendRequest('core.get_config', params, function(data) {
+      if (data) {
+        toR.resolve(data);
+      } else {
+        toR.reject(data);
+      }
+    });
 
-		params = [
-			[
-				"queue",
-				"name",
-				"total_size",
-				"state",
-				"progress",
-				"num_seeds",
-				"total_seeds",
-				"num_peers",
-				"total_peers",
-				"download_payload_rate",
-				"upload_payload_rate",
-				"eta",
-				"ratio",
-				"distributed_copies",
-				"is_auto_managed",
-				"time_added",
-				"tracker_host",
-				"save_path",
-				"total_done",
-				"total_uploaded",
-				"max_download_speed",
-				"max_upload_speed",
-				"seeds_peers_ratio"
-			],
-			[
+    return toR.promise;
+  }
 
-			]
-		];
+  function listTorrents(torrentHashes) {
+    const toR = Q.defer();
+    const params = [torrentHashes, [
+      'queue',
+      'name',
+      'total_size',
+      'state',
+      'progress',
+      'num_seeds',
+      'total_seeds',
+      'num_peers',
+      'total_peers',
+      'download_payload_rate',
+      'upload_payload_rate',
+      'eta',
+      'ratio',
+      'distributed_copies',
+      'is_auto_managed',
+      'time_added',
+      'tracker_host',
+      'save_path',
+      'total_done',
+      'total_uploaded',
+      'max_download_speed',
+      'max_upload_speed',
+      'seeds_peers_ratio',
+    ],
+    ];
 
-		send_request("web.update_ui", params, function(data){
-			if (data) {
-				toR.resolve(data);
-			} else {
-				toR.reject(data);
-			}
-		});
+    sendRequest('webapi.get_torrents', params, function(data) {
+      if (data) {
+        toR.resolve(data);
+      } else {
+        toR.reject(data);
+      }
+    });
 
-		return toR.promise;
-	}
+    return toR.promise;
+  }
 
-	function loggit(data)
-	{
-		if(data.result == true){
-			console.log("Successfully authenticated with Deluge client!");
-		}
-		else{
-			console.error("Deluge authentication failed, please check whether your hostname, password & port are correct!");
-		}
+  function addTorrent(content, location) {
+    const toR = Q.defer();
+    const params = [
+      content,
+      {
+        download_location: location // eslint-disable-line
+      },
+    ];
 
-	}
+    sendRequest('webapi.add_torrent', params, function(data) {
+      if (data) {
+        toR.resolve(data);
+      } else {
+        toR.reject(data);
+      }
+    });
 
-	
-	function auth()
-	{
-		var toR = Q.defer();
-		var params = [ password ];
-		send_request("auth.login", params, function(res) {
-			if (res) {
-				toR.resolve(res);
-			} else {
-				toR.reject(res);
-			}
-		});
+    return toR.promise;
+  }
 
-		return toR.promise;
-	}
 
-	function send_request(method, params, callback) {
+  function getStatus(callback) {
+    const toR = Q.defer();
+    const params = [
+      [
+        'queue',
+        'name',
+        'total_size',
+        'state',
+        'progress',
+        'num_seeds',
+        'total_seeds',
+        'num_peers',
+        'total_peers',
+        'download_payload_rate',
+        'upload_payload_rate',
+        'eta',
+        'ratio',
+        'distributed_copies',
+        'is_auto_managed',
+        'time_added',
+        'tracker_host',
+        'save_path',
+        'total_done',
+        'total_uploaded',
+        'max_download_speed',
+        'max_upload_speed',
+        'seeds_peers_ratio',
+      ],
+      [
 
-		//setup our request payload from input params
-		var payload ={
-			'method' : method,
-			'params' : params,
-			'id'     : counter++
-		};
+      ],
+    ];
 
-		//make payload a string for http request
-		payload = JSON.stringify(payload);
+    sendRequest('web.update_ui', params, function(data) {
+      if (data) {
+        toR.resolve(data);
+      } else {
+        toR.reject(data);
+      }
+    });
 
-		//set auth cookie if already authenticated
-		var head;
-		if(auth_cooke != ""){
-			head = {
-				'Cookie': auth_cooke
-			};
-		}
+    return toR.promise;
+  }
 
-		//setup our http request parameters
-		var options = {
-			hostname: hostname,
-			port: port,
-			path: '/json',
-			method: 'POST',
-			headers: head
-		};
+  function loggit(data) {
+    if(data.result == true) {
+      console.log('Successfully authenticated with Deluge client!');
+    } else {
+      console.error('Deluge authentication failed!');
+    }
 
-		var req = http.request(options, function(res) {
+  }
 
-			var gunzip = zlib.createGunzip();
-			res.pipe(gunzip);
 
-			if(auth_cooke == ""){
-				auth_cooke = JSON.stringify(res.headers['set-cookie']).slice(2,50);
-			}
+  function auth() {
+    const toR = Q.defer();
+    const params = [ password ];
+    sendRequest('auth.login', params, function(res) {
+      if (res && res.result) {
+        toR.resolve(res);
+      } else {
+        toR.reject(res);
+      }
+    });
 
-			var buffer = [];
-			gunzip.on('data', function (chunk) {
-				buffer.push(chunk.toString());
-			});
-			gunzip.on('end', function() {
-				callback(JSON.parse(buffer.join("")));
-			});
+    return toR.promise;
+  }
 
-		});
+  function sendRequest(method, params, callback) {
 
-		//output http request errors to console
-		req.on('error', function(e) {
-			console.log('problem with request: ' + e.message);
-		});
+    //setup our request payload from input params
+    let payload = {
+      'method': method,
+      'params': params,
+      'id': counter++,
+    };
 
-		// write data to request body
-		req.write(payload);
-		req.end();
+    //make payload a string for http request
+    payload = JSON.stringify(payload);
 
-	}
+    //set auth cookie if already authenticated
+    let head;
+    if(authCookie != '') {
+      head = {
+        'Cookie': authCookie,
+      };
+    }
 
-	return {
-		getConfig: get_config,
-		getStatus: get_status,
-		auth: auth,
-		close: close
-	};
-	
+    //setup our http request parameters
+    const options = {
+      hostname: hostname,
+      port: port,
+      path: '/json',
+      method: 'POST',
+      headers: head,
+    };
+
+    const req = http.request(options, function(res) {
+
+      const gunzip = zlib.createGunzip();
+      res.pipe(gunzip);
+
+      if(authCookie == '') {
+        authCookie = JSON.stringify(res.headers['set-cookie']).slice(2,50);
+      }
+
+      const buffer = [];
+      gunzip.on('data', function(chunk) {
+        buffer.push(chunk.toString());
+      });
+      gunzip.on('end', function() {
+        callback(JSON.parse(buffer.join('')));
+      });
+
+    });
+
+    //output http request errors to console
+    req.on('error', function(e) {
+      console.log('problem with request: ' + e.message);
+    });
+
+    // write data to request body
+    req.write(payload);
+    req.end();
+
+  }
+
+  return {
+    getConfig: getConfig,
+    getStatus: getStatus,
+    addTorrent: addTorrent,
+    listTorrents: listTorrents,
+    auth: auth,
+    close: close,
+  };
 };
