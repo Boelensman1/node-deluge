c7c0ebc960b3337725c258f279e7ecebeec3cb61
Boelensman1 <me@wiggerboelens.com>
2016-10-11 20:22:16 +0200

    Made tests actually test something and added tests for the new functions
    Might have been caused by the newer version of mocha, but previously they just went to OK immediatly even if it was not ok.
    
    Signed-off-by: Boelensman1 <me@wiggerboelens.com>

--- test/index.js
+++ test/index.js
@@ -1,21 +1,49 @@
-var should = require('chai').should(),
-    deluge = require('../index')('sd-80083.dedibox.fr', 'deluge', null);
+const fs = require('fs');
+const q = require('q');
+
+const should = require('chai').should();
+const deluge = require('../index')(
+  'server',
+  'pass',
+  0000 // port
+);
 
 describe('#get_config', function() {
-	it('gets the current status of the deluge client', function() {
-		deluge.auth().then(deluge.getConfig).then(function (data) {
-    			should.exist(data);
-  			data.should.be.an('object');
-    		});
-	});
+  it('gets the current status of the deluge client', function() {
+    return deluge.auth().then(deluge.getConfig).then(function(data) {
+      should.exist(data);
+      data.should.be.an('object');
+    });
+  });
 });
 
 describe('#get_status', function() {
-	it('gets the current config of the deluge client', function() {
-		deluge.auth().then(deluge.getStatus).then(function (data) {
-    			should.exist(data);
-  			data.should.be.an('object');
-		});
-				  
-	});
+  it('gets the current config of the deluge client', function() {
+    return deluge.auth().then(deluge.getStatus).then(function(data) {
+      should.exist(data);
+      data.should.be.an('object');
+    });
+  });
+});
+
+describe.skip('#add_torrent', function() {
+  it('add a torrent to the deluge client', function() {
+    this.timeout(10000);
+    return new q.Promise(function(resolve, reject) {
+
+      return deluge.auth().then(function() {
+        const name = './torrentfile';
+        return fs.readFile(name, function(err, torrent) {
+          if (err) { throw err; };
+          const torrentBase64 = torrent.toString('base64');
+          return resolve(
+            deluge.addTorrent(torrentBase64, 'location')
+            .then(function(result) {
+              result.should.be.an('object');
+            })
+          );
+        });
+      });
+    });
+  });
 });
